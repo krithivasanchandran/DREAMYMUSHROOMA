98e414ceed7c5ee6d8e17a734860e5452f9f08fe
krithivasan <krithivasanchandran@users.noreply.github.com>
2019-03-31 23:49:32 +0530

    Server Side logic persisting user data anonymously.

---
+++ DreamyMashrooma/WebContent/Gallery.jsp
@@ -0,0 +1,109 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Dreamy Mashrooma, Gallery of dreamers anonymous users
+	content. Privacy safe content sharing platform.</title>
+<link rel="shortcut icon" type="png"
+	href="https://img.icons8.com/cotton/64/000000/moon.png" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+body {
+	font-family: "Montserrat", sans-serif;
+	font-smoothing: antialiased;
+	background-color: #5774b8;
+}
+
+.headingTitle {
+	color: #F98866;
+	font-family: fantasy;
+	font-weight: normal;
+	margin : 15px 15px 15px 15px;
+	text-align: center;
+	padding : 15px 15px 15px 15px;
+}
+
+.good-review-score{
+    background-color:#66CC33;
+    color:white;
+    text-align: center;
+    padding: 10px;
+    margin-right: 5px
+}
+
+</style>
+</head>
+
+
+<body>
+
+
+	<h1 class="headingTitle">Dreamy Mushrooma :: User's Gallery</h1>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-block">
+						<h1 class="card-text good-review-score float-left">8.0</h1>
+						<h4 class="card-title">Card on the Left 1</h4>
+						<p class="card-text">With supporting text below as a natural
+							lead-in to additional content.</p>
+					</div>
+					<div class="card-footer">
+						<p class="card-text text-right">Footer Text - Possible Link
+							Out</p>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-block">
+						<h1 class="card-text good-review-score float-left">8.0</h1>
+						<h4 class="card-title">Card on the Left 2</h4>
+						<p class="card-text">With supporting text below as a natural
+							lead-in to additional content.</p>
+					</div>
+					<div class="card-footer">
+						<p class="card-text text-right">Footer Text - Possible Link
+							Out</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-block">
+						<h1 class="card-text good-review-score float-left">8.0</h1>
+						<h4 class="card-title">Card on the Right 1</h4>
+						<p class="card-text">With supporting text below as a natural
+							lead-in to additional content.</p>
+					</div>
+					<div class="card-footer">
+						<p class="card-text text-right">Footer Text - Possible Link
+							Out</p>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-block">
+						<h1 class="card-text good-review-score float-left">8.0</h1>
+						<h4 class="card-title">Card on the Right 1</h4>
+						<p class="card-text">With supporting text below as a natural
+							lead-in to additional content.</p>
+					</div>
+					<div class="card-footer">
+						<p class="card-text text-right">Footer Text - Possible Link
+							Out</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- DreamyMashrooma/src/com/dreamymushrooma/service/UserDreamPost.java
+++ DreamyMashrooma/src/com/dreamymushrooma/service/UserDreamPost.java
@@ -1,6 +1,11 @@
 package com.dreamymushrooma.service;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,9 +14,36 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class UserDreamPost
+ * 
+ * Example Request: krithi<div>asdada</div><div>sda</div><div>sdas</div><div><br></div><div>s</div>
  */
 @WebServlet("/UserDreamPost")
 public class UserDreamPost extends HttpServlet {
+	
+	private static final String unqiueId = "D";
+	private static final AtomicInteger Counter = new AtomicInteger(3212);
+	private static final String fillers  = 
+			"hello world this is krithivasan from paypal who like to work hard and have "+
+			"fun building things. Here is a to do way of doing things and hence to be better off";
+	
+	/*
+	 * Contents created by the user is persisted here: 
+	 * 
+	 *  Key : contentgenericUniqueId 
+	 *  Value : contents of the dream goes here
+	 */
+	
+	private static Map<String,String> dreamcontents = new ConcurrentHashMap<String,String>();
+	
+	/*
+	 * Contents created by the user is persisted here: 
+	 * 
+	 *  Key : username which is getting from okta. 
+	 *  Value : LinkedList of contentgenericUniqueId
+	 */
+	
+	private static Map<String,LinkedList<String>> dreamUserContentMapper = new ConcurrentHashMap<String,LinkedList<String>>(); 
+	
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -39,14 +71,76 @@ public class UserDreamPost extends HttpServlet {
 			throws ServletException, IOException {
 		doGet(request, response);
 		
-	//	Object o = request.getAttribute("contentdream");
+		String dreamcontent = request.getParameter("content");
 		
-	//	System.out.println(" Instance of : " + o.toString());
- 
-		String dreamcontent = request.getParameter("dreamcontents");
+		StringBuilder cleanText = new StringBuilder();
+		
+		//Escape Check to avoid null pointer exceptions
+		dreamcontent = ((dreamcontent.isEmpty()) || dreamcontent.length() == 0) ? fillers : dreamcontent;
+		
+        if(!dreamcontent.isEmpty() && dreamcontent.length() != 0 && dreamcontent != null){
+			
+			/*
+			 * Check for HTML tags appended to the string
+			 */
+			
+			final String openbraces = "<";
+			final String closebrace = ">";
+			
+			int firstindex = 0;
+			
+			while(dreamcontent.length() > 0 && dreamcontent.contains("<")){
+				
+				int startpos = dreamcontent.indexOf(openbraces, firstindex);
+				int endpos = dreamcontent.indexOf(closebrace, startpos);
+				
+				String dc = dreamcontent.substring(firstindex,startpos);
+				cleanText.append(dc);
+				
+				dreamcontent = dreamcontent.substring(endpos+1, dreamcontent.length());
+			}
+			
+			System.out.println(" Clean Text String :: " + cleanText.toString());
+			
+		}
 
-		System.out.println(" Dream Content in the Servlet :: => " + dreamcontent);
+		/*
+		 * Generating unique content id.
+		 */
+        int currVal = Counter.incrementAndGet();
+        String generUniqueId = unqiueId.concat(currVal+"");
+        
+        final String confidentialUserContent = cleanText.toString();
+        
+        dreamcontents.put(generUniqueId, confidentialUserContent.isEmpty() ? "Contents are empty" : confidentialUserContent);
+        Counter.set(currVal);
+        
+        if(dreamUserContentMapper.isEmpty()){
+        	
+        	LinkedList<String> userContentid = new LinkedList<String>();
+        	userContentid.addFirst(generUniqueId);
+        	dreamUserContentMapper.put("username", userContentid);
+        }
+        else if(dreamUserContentMapper.containsKey("username")){
+        	
+        	LinkedList<String> usercontentIdList = dreamUserContentMapper.get("username");
+        	usercontentIdList.addLast(generUniqueId);
+        	
+        }else{
+        	
+        	LinkedList<String> usercontentIdList = new LinkedList<String>();
+        	usercontentIdList.addFirst(generUniqueId);
+        	dreamUserContentMapper.put("username", usercontentIdList);
+        }
+		
+		response.setContentType("text/plain");
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().write(generUniqueId);  
 
+	}
+	
+	public static Map<String,String> getUserContents(){
+		return dreamcontents;
 	}
 
 }
